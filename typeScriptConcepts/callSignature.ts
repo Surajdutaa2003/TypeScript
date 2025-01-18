a6c8226bd0ca4ac0800e22acc7d6b7d3168f1e55
Surajdutaa2003 <Suraj.621dutta@gmail.com>
2025-01-18 12:32:26 +0530

    [Suraj] Implemented function Signature in TypeScript

---
+++ typeScriptConcepts/callSignature.ts
@@ -0,0 +1,9 @@
+// Function signature
+let addNumbers: (x: number, y: number) => number;
+
+// Function implementation
+addNumbers = (x, y) => {
+  return x + y;
+};
+
+console.log(addNumbers(5, 10)); // Output: 15

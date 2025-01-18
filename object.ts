48e512d5c2b16c12b8cc893e3187511e6029ed40
Surajdutaa2003 <Suraj.621dutta@gmail.com>
2025-01-18 10:51:35 +0530

    [Suraj] implementation of boject in TypeScript

---
+++ object.ts
@@ -0,0 +1,14 @@
+const person :{
+
+    name:string;
+    age:number;
+    isStudent:boolean;
+    address:{city:string;Country:string}
+}={
+    name:"Suraj",
+    age:23,
+    isStudent:true,
+    address:{city:"Dhanbad",Country:"India"}
+
+}
+console.log(person);

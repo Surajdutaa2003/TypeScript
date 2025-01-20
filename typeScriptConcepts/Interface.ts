72a2615a76669e795cf05a590b6cf2e84dd3c59f
Surajdutaa2003 <Suraj.621dutta@gmail.com>
2025-01-20 11:54:43 +0530

    [Suraj] Implemented Interface in TypeScript

---
+++ typeScriptConcepts/Interface.ts
@@ -0,0 +1,13 @@
+interface Products{
+    name:string;
+    price:number;
+    quantity:number
+
+}
+
+const product1:Products={
+    name:"vinod",
+    price:1000,
+    quantity:50
+}
+console.log(product1.name);
